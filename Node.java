05a0007065fee23648adbb8c27af774a95c34c39
Ëzo-Tanuki <michailbryanlie@gmail.com>
2023-04-26 15:40:15 +0700

    update node

--- Node.java
+++ Node.java
@@ -1,3 +1,18 @@
 public class Node {
     protected int color;
+    protected int degree;
+
+    public Node() {
+        this.color = -1;
+    }
+
+    public Node(int color) {
+        this.color = color;
+        this.degree = 0;
+    }
+
+    public void reset(){
+        this.color = -1;
+        this.degree = 0;
+    }
 }
