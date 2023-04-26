9d20cca1c3d8039a0c060cae5abca6a3e70db3c0
Ëzo-Tanuki <michailbryanlie@gmail.com>
2023-04-26 16:18:28 +0700

    quick fix

--- Graph.java
+++ Graph.java
@@ -1,31 +1,27 @@
+import java.util.ArrayList;
+
 public class Graph{
     final int size;
-    protected Node[] nodes;
+    protected ArrayList<Node> nodes;
 
     protected int edges[][];
 
     public Graph(int size) {
         this.size = size;
-        this.nodes = new Node[size];
+        this.nodes = new ArrayList<>(size);
         this.edges = new int[size][size];
     }
 
-    public Node[] getNodes() {
+    public ArrayList<Node> getNodes() {
         return nodes;
     }
 
     public int[][] getEdges() {
         return edges;
     }
-    
-    public class ConnectNode extends Graph {
-    public ConnectNode(int size) {
-        super(size);
-    }
 
-    public void connect(int node1, int node2) {
+    public void connectNodes(int node1, int node2) {
         this.edges[node1][node2] = 1;
         this.edges[node2][node1] = 1;
     }
-}
 }
