59a48a935da066a509cff73dbe945bd806bcdd12
Sulthan Azka <114067296+Narwhalssss@users.noreply.github.com>
2023-04-26 15:38:54 +0700

    Update Graph.java

--- Graph.java
+++ Graph.java
@@ -1,6 +1,6 @@
 public class Graph{
     int size;
-    protected Node nodes[];
+    protected Node[] nodes;
 
     protected int vertex[][];
 
@@ -9,6 +9,28 @@ public class Graph{
         this.nodes = new Node[size];
         this.vertex = new int[size][size];
     }
-    
-    
+
+    public int getSize() {
+        return size;
+    }
+
+    public Node[] getNodes() {
+        return nodes;
+    }
+
+    public int[][] getVertex() {
+        return vertex;
+    }
+
+    public void setSize(int size) {
+        this.size = size;
+    }
+
+    public void setNodes(Node[] nodes) {
+        this.nodes = nodes;
+    }
+
+    public void setVertex(int[][] vertex) {
+        this.vertex = vertex;
+    }
 }
