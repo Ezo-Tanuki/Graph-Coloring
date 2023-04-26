e99fe629f778b982a647b29e480332776df39686
Ëzo-Tanuki <michailbryanlie@gmail.com>
2023-04-26 15:46:15 +0700

    remove unecessary methods

--- Graph.java
+++ Graph.java
@@ -10,27 +10,7 @@ public class Graph{
         this.vertex = new int[size][size];
     }
 
-    public int getSize() {
-        return size;
-    }
-
-    public Node[] getNodes() {
-        return nodes;
-    }
-
     public int[][] getVertex() {
         return vertex;
-    }
-
-    public void setSize(int size) {
-        this.size = size;
-    }
-
-    public void setNodes(Node[] nodes) {
-        this.nodes = nodes;
-    }
-
-    public void setVertex(int[][] vertex) {
-        this.vertex = vertex;
     }
 }
