5bd0ddd0694bc2776de77b1dc1d1ee9f9f60bc71
Ëzo-Tanuki <michailbryanlie@gmail.com>
2023-05-02 18:03:04 +0700

    Clean up unused code

--- Graph.java
+++ Graph.java
@@ -1,35 +1,39 @@
 import java.util.ArrayList;
+import java.util.Map;
 
 public class Graph{
     private final int MAX_SIZE;
     protected int size;
     private ArrayList<Node> nodes;
+    private Map<Integer, String> colorSchemeMap;
 
-    // protected int edges[][];
-
-    public Graph(){
-        this.MAX_SIZE = -1;
+    public Graph() {
+        this.MAX_SIZE = Integer.MAX_VALUE;
         this.size = 0;
         this.nodes = new ArrayList<>();
     }
 
-    public Graph(int maxSize) {
-        this.MAX_SIZE = Integer.MAX_VALUE;
+    public Graph(Map<Integer, String> colorSchemeMap, int maxSize) {
+        this.MAX_SIZE = maxSize;
         this.size = 0;
         this.nodes = new ArrayList<>(size);
-        // this.edges = new int[size][size];
+        this.setColorSchemeMap(colorSchemeMap);
     }
 
-    public ArrayList<Node> getNodes() {
-        return nodes;
+    public Graph(int maxSize, Map<Integer, String> colorSchemeMap) {
+        this.MAX_SIZE = maxSize;
+        this.size = 0;
+        this.nodes = new ArrayList<>(size);
+        this.setColorSchemeMap(colorSchemeMap);
     }
 
-    // public int[][] getEdges() {
-    //     return edges;
-    // }
+    public Graph(int maxSize) {
+        this.MAX_SIZE = maxSize;
+        this.size = 0;
+        this.nodes = new ArrayList<>(size);
+    }
 
-
-    public boolean addNode(){
+    public boolean addNode() {
         if(this.size >= this.MAX_SIZE){
             System.out.println("Max size reached!");
             return false;
@@ -51,9 +55,24 @@ public class Graph{
         node2.addConnection(node1);
     }
 
-    public void printColor(){
+    public void printColor() {
         for(Node node: this.nodes){
-            System.out.println(node.getIdentifier() + ": " + node.color);
+            if(colorSchemeMap != null){
+                System.out.println(node.getIdentifier() + ": " + colorSchemeMap.get(node.getColor()));
+            }
+            
+            else System.out.println(node.getIdentifier() + ": " + node.getColor());
         }
     }
+
+    
+
+    public void setColorSchemeMap(Map<Integer, String> colorSchemeMap) {
+        this.colorSchemeMap = colorSchemeMap;
+    }
+
+    public ArrayList<Node> getNodes() {
+        return nodes;
+    }
+
 }
